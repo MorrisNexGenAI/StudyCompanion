f989d90c6416c8be25b63c6106ddef04596143e2
Morris <soulvibe.com@gmail.com>
2026-01-17 21:55:55 +0000

    add the save form

--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+
+# Create your models here.
+from django.db import models
 from django.utils import timezone
 
 class BaseModel(models.Model):
+    id = models.BigAutoField(primary_key=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         abstract = True
         ordering = ['-created_at']
